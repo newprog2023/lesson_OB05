c1e4969c513a9c55327002faa1ee23125ce92415
newprog2023 <newprog@internet.ru>
2024-08-18 18:49:54 +0300

    go from mouse and keys

--- main.py
+++ main.py
@@ -5,13 +5,34 @@ window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Заголовок главного окна")
 
+image = pygame.image.load("target.png")
+image_rect = image.get_rect()
+
+speed = 1
+
 run = True
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
-    screen.fill((0, 0, 0))
-    pygame.display.flip
+        if event.type == pygame.MOUSEMOTION:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            image_rect.x = mouseX
+            image_rect.y = mouseY
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        image_rect.x -= speed
+    if keys[pygame.K_RIGHT]:
+        image_rect.x += speed
+    if keys[pygame.K_UP]:
+        image_rect.y -= speed
+    if keys[pygame.K_DOWN]:
+        image_rect.y += speed
+
+    screen.fill((30, 70, 100))
+    screen.blit(image, image_rect)
+    pygame.display.flip()
 
 pygame.quit()
